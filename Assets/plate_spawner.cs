3df07b9b254ca5b6b596b2d27e1ca97ef0fc97d3
ksuber14 <ksuber14@users.noreply.github.com>
2018-05-08 16:42:48 -0400

    added enmey spawner

---
+++ Assets/plate_spawner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plate_spawner : MonoBehaviour {
+    public GameObject Plate;
+    float randx;
+    Vector2 wheretospawn;
+    public float spawnrate = 2f;
+    float nextspawn = 1.0f;
+    private static object Random;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextspawn)
+        { nextspawn = Time.time + spawnrate;
+            float randx = (5.08f - 5.08f);
+            wheretospawn = new Vector2(randx, transform.position.y);
+            Instantiate(Plate, wheretospawn, Quaternion.identity);
+	}
+}
+    
+    
+        
+    }
+
+
